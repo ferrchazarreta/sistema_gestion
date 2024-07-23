b9bb2320163a2c9e2c271474e839d9430bd2c084
Mateo Gurrea <mategurrea@gmail.com>
2024-07-23 10:33:20 -0300

    creando modelos necesarios para vehiculos

--- vehiculos/models.py
+++ vehiculos/models.py
@@ -1,3 +1,74 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
 from django.db import models
 
+class Model(models.Model):
+  name = models.CharField(max_length=100)
+  
+  def __str__(self):
+    return  self.name
+
+class Brand(models.Model):
+  name = models.CharField(max_length=100)
+  
+  def __str__(self):
+    return  self.name
+
+class Fuel(models.Model):
+  name = models.CharField(max_length=10)
+  
+  def __str__(self):
+    return  self.name
+
+class Country(models.Model):
+  name = models.CharField(max_length=10)
+  
+  def __str__(self):
+    return  self.name
+
+
 # Create your models here.
+class Product(models.Model):
+
+  brand = models.ForeignKey(
+    Brand,
+    on_delete=models.SET_NULL,
+    related_name='products',
+  )
+
+  model_car = models.ForeignKey(
+    Model,
+    on_delete=models.SET_NULL,
+    related_name='products',
+  )
+
+  year_production = models.TextField(
+    max_digits=4
+  )
+
+  door_quatity = models.IntegerField(
+    max_digits=1
+  )
+
+  cilindrada = models.IntegerField(
+    max_digits=2
+  )
+
+  fuel_type = models.ForeignKey(
+    Fuel,
+    on_delete=models.SET_NULL,
+    related_name='products',
+  )
+
+  country_production = models.ForeignKey(
+    Country,
+    on_delete=models.SET_NULL,
+    related_name='products',
+  )
+
+  price = models.IntegerField(
+    max_digits=50
+  )
+
+  def __str__(self):
+    return  self.name_model
